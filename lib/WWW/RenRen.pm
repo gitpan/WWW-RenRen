ee5a3c967e803fba187e9fdddbcca0a6e464d3d2
Aaron Lewis <codebreak@cpan.org>
2012-07-27 10:46:05 +0000

    Import of CODEBREAK/WWW-RenRen-0.01 from CPAN.
    gitpan-cpan-distribution: WWW-RenRen
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         CODEBREAK/WWW-RenRen-0.01.tar.gz
    gitpan-cpan-author:       CODEBREAK
    gitpan-cpan-maturity:     released

---
+++ lib/WWW/RenRen.pm
@@ -0,0 +1,111 @@
+package WWW::RenRen;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+WWW::RenRen - The great new WWW::RenRen!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use WWW::RenRen;
+
+    my $foo = WWW::RenRen->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Aaron Lewis, C<< <the.warl0ck.1989 at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-www-renren at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=WWW-RenRen>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc WWW::RenRen
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=WWW-RenRen>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/WWW-RenRen>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/WWW-RenRen>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/WWW-RenRen/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2012 Aaron Lewis.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of WWW::RenRen
